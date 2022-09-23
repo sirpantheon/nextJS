ac5c10181734eb74037905a64ae860c19eaa1760
johnatan sousa paixao <johnatan.sousa@gmail.com>
2022-09-23 19:55:42 -0300

    autentificação

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,46 +1,60 @@
-import React from "react"
-import {useRouter} from "next/router"
+import React from "react";
+import { useRouter } from "next/router";
+import { authService } from "../src/services/auth/authServices";
 
 export default function HomeScreen() {
-  const [values,setValues] = React.useState({
-    usuario:"omariosouto",
-    senha:"safepassword",
-  })
-  const router = useRouter()
+  const [values, setValues] = React.useState({
+    usuario: "omariosouto",
+    senha: "safepassword",
+  });
+  const router = useRouter();
 
   function handleChange(event) {
-    const fieldValue = event.target.value
-    const fieldName = event.target.name
+    const fieldValue = event.target.value;
+    const fieldName = event.target.name;
 
-    setValues((currentValue)=>{
-      return{
+    setValues((currentValue) => {
+      return {
         ...currentValue,
-        [fieldName]: fieldValue
+        [fieldName]: fieldValue,
       };
-    })
+    });
   }
 
   return (
     <div>
       <h1>Login</h1>
-      <form onSubmit={(event)=>{
-        event.preventDefault()
-
-        router.push("/auth-page-ssr")
-        //router.push("/auth-page-static")
-      }}>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          authService
+            .login({
+              username: values.usuario,
+              password: values.password,
+            })
+            .then(() => {
+              router.push("/auth-page-ssr");
+              //router.push("/auth-page-static")
+            }).catch(() => {
+              alert("Usuario ou senha são invalidos");
+            })
+        }}
+      >
         <input
-          placeholder="Usuário" name="usuario"
-          value={values.usuario} onChange={handleChange}
+          placeholder="Usuário"
+          name="usuario"
+          value={values.usuario}
+          onChange={handleChange}
         />
         <input
-          placeholder="Senha" name="senha" type="password"
-          value={values.senha} onChange={handleChange}
+          placeholder="Senha"
+          name="senha"
+          type="password"
+          value={values.senha}
+          onChange={handleChange}
         />
         <div>
-          <button>
-            Entrar
-          </button>
+          <button>Entrar</button>
         </div>
       </form>
     </div>
---
+++ frontend/src/services/auth/authServices.jsx
@@ -0,0 +1,17 @@
+export const authService = {
+  async login(username, password) {
+    return fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/login`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+    })
+    .then(async (response) => {
+      if (!response.ok) throw new Error("login não autorizado!");
+      const body = await response.json();
+      console.log(body);
+    });
+  },
+};
